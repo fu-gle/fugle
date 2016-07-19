0a0f4df1952cf7aa36b0687a47d96b645344546b
uwangg <uwangg@gmail.com>
2016-07-20 02:08:16 +0900

    회원가입에서 핸드폰 갤러리에서 image 받기 완료 (사진크이,모양수정까지 완료)

---
+++ app/src/main/java/kr/fugle/login/EmailLoginActivity.java
@@ -0,0 +1,15 @@
+package kr.fugle.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import kr.fugle.R;
+
+public class EmailLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_login);
+    }
+}
--- app/src/main/java/kr/fugle/register/RegisterActivity.java
+++ app/src/main/java/kr/fugle/register/RegisterActivity.java
@@ -1,5 +1,7 @@
 package kr.fugle.register;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -7,21 +9,40 @@ import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+
 import kr.fugle.R;
+import kr.fugle.login.CircleTransform;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-    private EditText inputName, inputEmail, inputPassword;
-    private TextInputLayout inputLayoutName, inputLayoutEmail, inputLayoutPassword;
+    private EditText inputName, inputEmail, inputPassword, inputMessage;
+    private TextInputLayout inputLayoutName, inputLayoutEmail, inputLayoutPassword,
+            inputLayoutMessage, inputLayoutImage;
     private Button btnSignUp;
+    private ImageView inputImage;
+
+    // 갤러리에서 사진가져오기
+    private int REQ_PICK_CODE = 100;
+
+    // 서버 통신 OkHttp
+    final static String serverUrl = "http://52.79.147.163:8000/";
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,23 +50,28 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        if(toolbar != null) {
+        if (toolbar != null) {
             setSupportActionBar(toolbar);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-
+        inputLayoutImage = (TextInputLayout) findViewById(R.id.input_layout_profileimg);
         inputLayoutName = (TextInputLayout) findViewById(R.id.input_layout_name);
         inputLayoutEmail = (TextInputLayout) findViewById(R.id.input_layout_email);
         inputLayoutPassword = (TextInputLayout) findViewById(R.id.input_layout_password);
+        inputLayoutMessage = (TextInputLayout) findViewById(R.id.input_layout_message);
+
+        inputImage = (ImageView) findViewById(R.id.input_profileimg);
         inputName = (EditText) findViewById(R.id.input_name);
         inputEmail = (EditText) findViewById(R.id.input_email);
         inputPassword = (EditText) findViewById(R.id.input_password);
+        inputMessage = (EditText) findViewById(R.id.input_message);
         btnSignUp = (Button) findViewById(R.id.btn_signup);
 
         inputName.addTextChangedListener(new MyTextWatcher(inputName));
         inputEmail.addTextChangedListener(new MyTextWatcher(inputEmail));
         inputPassword.addTextChangedListener(new MyTextWatcher(inputPassword));
+        inputMessage.addTextChangedListener(new MyTextWatcher(inputMessage));
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +79,30 @@ public class RegisterActivity extends AppCompatActivity {
                 submitForm();
             }
         });
+
+        // 갤러리에서 사진가져오기
+        inputImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickerIntent = new Intent(Intent.ACTION_PICK);
+                pickerIntent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+                pickerIntent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(pickerIntent, REQ_PICK_CODE);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        CircleTransform circleTransform = new CircleTransform();
+        Picasso.with(this)
+                .load(data.getData())
+                .resize(800, 600)
+                .transform(circleTransform)
+                .into(inputImage);
     }
 
     @Override
@@ -81,6 +131,14 @@ public class RegisterActivity extends AppCompatActivity {
             return;
         }
 
+        // name, email, password, message
+        new OkHttpLogin().execute(
+                serverUrl,
+                inputName.getText().toString(),
+                inputEmail.getText().toString(),
+                inputPassword.getText().toString(),
+                inputMessage.getText().toString()
+        );
         Toast.makeText(getApplicationContext(), "Thank You!", Toast.LENGTH_SHORT).show();
     }
 
@@ -158,6 +216,48 @@ public class RegisterActivity extends AppCompatActivity {
                     validatePassword();
                     break;
             }
+        }
+    }
+
+    // 서버 통신
+    private class OkHttpLogin extends AsyncTask<String, Void, String> {
+
+        public final MediaType HTML = MediaType.parse("application/x-www-form-urlencoded; charset=utf-8");
+
+        @Override
+        protected String doInBackground(String... params) {
+            // 서버로 보낼 사용자 데이터
+            // 0: server address, 1: primary number, 2: nickname, 3: profileImgPath
+            String data = "name=" + params[1] + "&email=" + params[2]
+                    + "&password=" + params[3] + "&message=" + params[4] + "&image=" + params[5];  // 변경 필요
+            Log.d("OkHttpLogin.data", data);
+
+            RequestBody body = RequestBody.create(HTML, data);
+
+            Request request = new Request.Builder()
+                    .url(params[0] + "login/")     // 임시 로그인 주소
+                    .post(body)
+                    .build();
+
+            Log.d("OkHttpLogin.request", request.toString());
+
+            try {
+                // 서버로 전송
+                Response response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            // 서버에서 로그인 성공여부 받음
+            // 성공시 startActivity. 실패시 토스트 메세지
+            Log.d("ho's activity", "LoginActivity.OkHttpLogin.onPostExecute " + s);
         }
     }
 }
