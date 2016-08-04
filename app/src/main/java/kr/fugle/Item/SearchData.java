a46b4092d184a134d4cebe9023ee983733d25ad8
uwangg <uwangg@gmail.com>
2016-08-04 22:56:04 +0900

    searchData 싱글톤 만듦,

---
+++ app/src/main/java/kr/fugle/Item/SearchData.java
@@ -0,0 +1,29 @@
+package kr.fugle.Item;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 김은진 on 2016-08-04.
+ */
+// 작가명, 장르명
+public class SearchData {
+
+    private static SearchData searchData;
+
+    ArrayList<String> searchName;
+
+    private SearchData() {
+        searchName = new ArrayList<String>();
+    }
+
+    public static SearchData getInstance() {
+        if(searchData == null) {
+            searchData = new SearchData();
+        }
+        return searchData;
+    }
+
+    public ArrayList<String> getList() {
+        return searchName;
+    }
+}
