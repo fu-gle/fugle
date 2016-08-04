3670b269e079fa70a6fe2b7159e47ff640795d67
uwangg <uwangg@gmail.com>
2016-08-04 22:06:16 +0900

    검색하기 기능 자동완성 구현했음

--- app/src/main/java/kr/fugle/search/SearchActivity.java
+++ app/src/main/java/kr/fugle/search/SearchActivity.java
@@ -1,20 +1,18 @@
 package kr.fugle.search;
 
-import android.app.SearchManager;
-import android.content.ComponentName;
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import kr.fugle.R;
 
 public class SearchActivity extends AppCompatActivity {
+
+    String[] items = { "SM3", "SM5", "SM7", "SONATA", "AVANTE", "SOUL", "K5",
+            "K7" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,40 +20,57 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.search_toolbar);
+        toolbar.setTitle("검색하기");
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        AutoCompleteTextView edit = (AutoCompleteTextView) findViewById(R.id.edit);
+
+        edit.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, items));
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.search, menu);
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(new ComponentName(getApplicationContext(), SearchActivity.class)));
-        searchView.setMaxWidth(Integer.MAX_VALUE);
-        MenuItemCompat.expandActionView(menu.findItem(R.id.action_search));
-        searchView.setIconifiedByDefault(true);
-        searchView.setIconified(false);
-
-        MenuItem searchItem = menu.findItem(R.id.action_search);
-
-        MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
-            @Override
-            public boolean onMenuItemActionExpand(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
                 return true;
-            }
-
-            @Override
-            public boolean onMenuItemActionCollapse(MenuItem item) {
-                // TODO do your stuff when back button is pressed
-//                Intent intent = new Intent(SearchActivity.this, MainActivity.class);
-//                startActivity(intent);
-                finish();
-                return true;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
+        }
+        return super.onOptionsItemSelected(item);
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.search, menu);
+//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+//
+//        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+//        searchView.setSearchableInfo(searchManager.getSearchableInfo(new ComponentName(getApplicationContext(), SearchActivity.class)));
+//        searchView.setMaxWidth(Integer.MAX_VALUE);
+//        MenuItemCompat.expandActionView(menu.findItem(R.id.action_search));
+//        searchView.setIconifiedByDefault(true);
+//        searchView.setIconified(false);
+//
+//        MenuItem searchItem = menu.findItem(R.id.action_search);
+//
+//        MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
+//            @Override
+//            public boolean onMenuItemActionExpand(MenuItem item) {
+//                return true;
+//            }
+//
+//            @Override
+//            public boolean onMenuItemActionCollapse(MenuItem item) {
+//                // TODO do your stuff when back button is pressed
+////                Intent intent = new Intent(SearchActivity.this, MainActivity.class);
+////                startActivity(intent);
+//                finish();
+//                return true;
+//            }
+//        });
+//
+//        return super.onCreateOptionsMenu(menu);
+//    }
 }
