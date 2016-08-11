2a7c70c6dca0c5d3bc92cf87ac114eaf5a37052c
김호경 <ghrud92@gmail.com>
2016-08-11 09:56:29 +0900

    코멘트 액티비티 틀 만드는중

---
+++ app/src/main/java/kr/fugle/comment/CommentActivity.java
@@ -0,0 +1,40 @@
+package kr.fugle.comment;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import kr.fugle.R;
+
+public class CommentActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+
+        // 이전 액티비티에서 작품 번호, 작품 이름, 내 별점 받아옴
+        Intent data = getIntent();
+
+        // 툴바 생성
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(data.getStringExtra("title"));
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
