cc784889394717938fbd7fc069de9438e6e1abf0
uwangg <uwangg@gmail.com>
2016-08-22 18:40:59 +0900

    첫 로그인시 평가받는 부분 구현완료!(TutorialActivity) 첫 로그인인지 아닌지 구분해주는거 구현해야함..........후....

--- app/src/main/java/kr/fugle/Intro/TutorialActivity.java
+++ app/src/main/java/kr/fugle/Intro/TutorialActivity.java
@@ -1,13 +1,16 @@
 package kr.fugle.Intro;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.wooplr.spotlight.prefs.PreferencesManager;
 
+import kr.fugle.Item.User;
 import kr.fugle.R;
+import kr.fugle.rating.RatingActivity;
 
 /**
  * Created by 김은진 on 2016-08-19.
@@ -21,14 +24,27 @@ public class TutorialActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
 
-        mPreferencesManager = new PreferencesManager(TutorialActivity.this);
+        TextView goStarOk = (TextView) findViewById(R.id.go_star_ok);
+        TextView goStarNo = (TextView) findViewById(R.id.go_star_no);
+        TextView starText = (TextView) findViewById(R.id.star_text);
 
-        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.rootLayout);
-        relativeLayout.setOnClickListener(new View.OnClickListener() {
+        goStarOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TutorialActivity.this, RatingActivity.class));
+                finish();
+            }
+        });
+
+        goStarNo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        starText.setText(User.getInstance().getName()+
+                "님,"+ "\n" + "툰데레에게 취향을 알려주세요.");
+
     }
 }
